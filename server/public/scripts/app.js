ad2274e536344affd52c47779182d86678047d87
jpoehler <jpoehler@comcast.net>
2016-07-14 20:46:38 -0500

    styling changes

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -4,9 +4,9 @@ $(document).ready(function(){
 
       autoplay: 3000, //Set AutoPlay to 3 seconds
 
-      items : 3,
-      itemsDesktop : [1199,2],
-      itemsDesktopSmall : [979,2]
+      items : 1,
+      itemsDesktop : [1199,1],
+      itemsDesktopSmall : [979,1]
 
   });
 

dcb947d5d23429b705e8d0ba1ea3725b417365b6
jpoehler <jpoehler@comcast.net>
2016-08-06 21:15:07 -0500

    loop working

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -4,7 +4,8 @@ $(document).ready(function(){
 
       autoplay: 3000, //Set AutoPlay to 3 seconds
 
-      items : 1,
+      items: 1,
+      loop:true,
       itemsDesktop : [1199,1],
       itemsDesktopSmall : [979,1]
 
